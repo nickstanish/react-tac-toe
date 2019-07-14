062fcfdb61d6409d650775bd5c33422e7898a64b
Nick Stanish <nickstanish@gmail.com>
2019-07-14 11:56:57 -0500

    Upgrade dependencies

---
+++ docs/precache-manifest.c12d4fbe9fe3bd0b8998eed8af1e6037.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2545bee625bbf9c7e70cf599666097d",
+    "url": "/react-tac-toe/index.html"
+  },
+  {
+    "revision": "9dc9f6b7e909b967d3e3",
+    "url": "/react-tac-toe/static/css/main.2ad40348.chunk.css"
+  },
+  {
+    "revision": "7c364f425d439472b673",
+    "url": "/react-tac-toe/static/js/2.543d4b97.chunk.js"
+  },
+  {
+    "revision": "9dc9f6b7e909b967d3e3",
+    "url": "/react-tac-toe/static/js/main.45f82f46.chunk.js"
+  },
+  {
+    "revision": "af5767297c178fdff296",
+    "url": "/react-tac-toe/static/js/runtime~main.31049fbf.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-tac-toe/precache-manifest.b88e6ea540f2bdee9ba5ecc0013657aa.js"
+  "/react-tac-toe/precache-manifest.c12d4fbe9fe3bd0b8998eed8af1e6037.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-tac-toe/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-tac-toe/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
